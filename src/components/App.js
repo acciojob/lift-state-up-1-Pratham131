2f52f1f715dced2a262c618c92e757d75010ca1d
Pratham131 <prathamakhand2000@gmail.com>
2024-01-24 18:42:50 +0530

    update 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,13 @@
 import React from "react";
 import './../styles/App.css';
 import Parent from "./Parent";
-import Child from "./Child";
+
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
         <Parent/>
-        <Child/>
     </div>
   )
 }
