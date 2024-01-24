b6463a5911fd86abb5aace939ddff83e38f100b8
Pratham131 <prathamakhand2000@gmail.com>
2024-01-24 18:38:58 +0530

    first commit

---
+++ src/components/Child.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Child = ({showModal,setShowModal}) => {
+    return(
+        <div>
+            <h2> Child Component </h2>
+
+            <button onClick={() => setShowModal(true)}> Show Modal</button>
+
+            {
+                showModal==true && (
+                    <div>
+                        <h1> Modal Content </h1>
+                        <p> Thus is the modal content.</p>
+                    </div>
+                )
+            }
+        </div>
+    )
+} 
+
+export default Child;
